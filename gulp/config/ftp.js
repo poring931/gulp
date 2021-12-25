0cb523e9d09f19392a30b63c652a8a8d25be44fd
poring931 <poring.m@mail.ru>
2021-12-25 22:11:03 +0300

    Base for gulp build html\js\scss\webp

--- gulp/config/ftp.js
+++ gulp/config/ftp.js
@@ -1,6 +1,6 @@
 export let configFTP = {
-    host: "loftdcq6.beget.tech", // ftp address
-    user: "loftdcq6_test", // 
-    password: "DBl%LhS4", // ftp address
+    host: "", // ftp address
+    user: "", // 
+    password: "", // ftp address
     parallel: 5, // количество одновременных потоков
 }
